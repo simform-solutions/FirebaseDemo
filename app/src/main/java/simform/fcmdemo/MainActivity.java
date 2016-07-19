4d2108fbde6ee1591d43f7fd2e4c89e67063ae41
Sanket Kachhela <Sanket.Kachhela@Simforms-iMac-103.local>
2016-07-19 19:21:48 +0530

    FireBase Cloud Messaging Demo

---
+++ app/src/main/java/simform/fcmdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package simform.fcmdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseInstanceId.getInstance().getToken();
+    }
+}
